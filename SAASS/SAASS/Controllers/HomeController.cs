7aea1ae57bd89b341104dbd5bd7428689e41a953
Aysu Mammadova <tu7tw2d21@aztu.local>
2022-06-10 15:57:21 +0400

    add

---
+++ SAASS/SAASS/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SAASS.DAL;
+using SAASS.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SAASS.Controllers
+{
+    public class HomeController : Controller
+    {
+        private AppDbContext _context;
+
+        public HomeController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            List<Slider> sliders = _context.Sliders.ToList();
+            return View(sliders);
+        }
+
+       
+    }
+}
---
+++ SAASS/SAASS/Models/Slider.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SAASS.Models
+{
+    public class Slider
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Desc { get; set; }
+        public string Img { get; set; }
+    }
+}
